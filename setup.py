c155d00c6f3f76a9f0e03fd9217a43db9fb91990
Madhav Vijayan <39261345+madhavkrishnan@users.noreply.github.com>
2023-10-10 19:20:43 +1100

    bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gpulse',
-    version='0.1.0',
+    version='0.1.1',
     description='Genetic algorthm based optimiser for quantum control pulses',
     author='Madhav Krishnan Vijayan',
     author_email = 'mkv.215@gmail.com',
